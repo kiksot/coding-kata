3ff3c4827f073046f464b69313d84e47fe3f0de4
Nikolay Shurkalin <9460355@gmail.com>
2021-03-02 17:53:10 +0100

    fix: prettier added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+    },
+    env: {
+        node: true,
+    },
 };
